2eb5a026b725fd86f515b871a90afb8c95fc6ac9
dilesoft <dilesoft@gmail.com>
2025-06-11 17:59:42 +0300

    no root

--- index.ts
+++ index.ts
@@ -1,13 +1,16 @@
 /* License: Apache 2.0. https://www.apache.org/licenses/LICENSE-2.0 */
 
 import graphqlServer from './src/api/graphql/server';
+import config from './src/domain/config/config';
 import { initMikroOrm } from './src/features/db/mikro-orm';
 import NodeRepo from './src/features/repository/megapolos.node.repository';
 import UserRepo from './src/features/repository/user/user.repository';
 
-if (process.getuid() != 0) {
-  console.trace('You must run this app as root');
-  process.exit(1);
+if (!config.noRoot) {
+  if (process.getuid() != 0) {
+    console.trace('You must run this app as root');
+    process.exit(1);
+  }
 }
 
 export const megapolosPath = __dirname;
