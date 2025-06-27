3c04b76b5099d3f87d95d108248742ddbbe27ae6
n.kolesnikov <nick206265@gmail.com>
2025-06-27 16:27:08 +0300

    CREATE TABLE script

---
+++ install/test_case.sql
@@ -0,0 +1,19 @@
+-- Table: public.test_case
+
+-- DROP TABLE IF EXISTS public.test_case;
+
+CREATE TABLE IF NOT EXISTS public.test_case
+(
+    name character varying COLLATE pg_catalog."default",
+    title character varying COLLATE pg_catalog."default",
+    description character varying COLLATE pg_catalog."default",
+    id uuid NOT NULL DEFAULT gen_random_uuid(),
+    create_date timestamp without time zone DEFAULT now(),
+    update_date timestamp without time zone,
+    CONSTRAINT test_case_pkey PRIMARY KEY (id)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.test_case
+    OWNER to postgres;
