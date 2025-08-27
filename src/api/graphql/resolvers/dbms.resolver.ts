f6ef10a402a2c7fce1c933ad4b245f175de63173
akerom <zaboevi@gmail.com>
2025-08-27 16:29:05 +0300

    fix getDbUser double declration

--- src/api/graphql/resolvers/dbms.resolver.ts
+++ src/api/graphql/resolvers/dbms.resolver.ts
@@ -76,11 +76,6 @@ export class DbmsResolver {
     return (await DbmsRepo.getById(id)).getEntity();
   }
 
-  @Query(() => [DbUser])
-  async getDbUser(@Arg('id') id: string, @Ctx() ctx: Context): Promise<DbUser> {
-    return new DbUserRepo(ctx, id).getEntity();
-  }
-
   @Query(() => [String])
   async compareDbs(
     @Arg('dbId1') dbId1: string,
