29238891c7b624a20c8e5180dec144723868d174
akerom <zaboevi@gmail.com>
2025-09-04 16:54:32 +0300

    replace docker register connection from config file to get entiny

--- src/features/repository/docker.registry.repository.ts
+++ src/features/repository/docker.registry.repository.ts
@@ -1,6 +1,7 @@
 import { DockerRegistry } from '../../domain/entities/DockerRegistry.entity';
 import BaseRepo from './base.repository';
 import { makeEm } from './../db/mikro-orm';
+import config from '../../domain/config/config';
 
 export default class DockerRegistryRepo extends BaseRepo<DockerRegistry> {
   get entityClass() {
@@ -20,5 +21,19 @@ export default class DockerRegistryRepo extends BaseRepo<DockerRegistry> {
     });
 
     return true;
+  }
+
+  async getDefault(): Promise<DockerRegistry | null> {
+    const registries = await this.getByFields({ isDefault: true });
+    const defaultDockerRegistry = registries?.[0] ?? null;
+
+    return (
+      defaultDockerRegistry ??
+      Object.assign(new DockerRegistry(), {
+        host: config.registryHost,
+        user: config.registryUser,
+        password: config.registryPassword,
+      })
+    );
   }
 }
