72682408811390f4a05fe7e315f1a7a3a97b739d
n.kolesnikov <nick206265@gmail.com>
2025-09-04 20:15:13 +0300

    update resolver

--- src/api/graphql/resolvers/app.version.resolver.ts
+++ src/api/graphql/resolvers/app.version.resolver.ts
@@ -19,6 +19,7 @@ import {
   GenerationType,
 } from '../../../library/graphql_types_generator';
 import { ImageInput } from './image.resolver';
+import { makeEm } from '../../../features/db/mikro-orm';
 
 export const AppVersionInput = generateGraphQLInputType(
   AppVersion,
@@ -58,5 +59,14 @@ export class AppVersionResolver extends CreateBaseResolver(
   ): Promise<AppVersion> {
     const repo = new AppVersionRepo(ctx);
     return repo.createAppVersion(app_version_data, images);
+  }
+}
+
+@Resolver(() => AppVersion)
+export class AppVersionTableResolver extends BaseTableResolver {
+  @FieldResolver(() => [Image])
+  async images(@Root() appVersion: AppVersion): Promise<Image[]> {
+    const em = makeEm();
+    return await em.find(Image, { appVersions: { id: appVersion.id } });
   }
 }
