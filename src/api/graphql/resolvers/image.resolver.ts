9ff2e50495d983f7b6d2621d4773989530fcad6a
akerom <zaboevi@gmail.com>
2025-09-10 08:23:05 +0300

    fix catch global fail buildImage

--- src/api/graphql/resolvers/image.resolver.ts
+++ src/api/graphql/resolvers/image.resolver.ts
@@ -54,7 +54,7 @@ export class ImageResolver extends CreateBaseResolver(
     @Arg('imageId') imageId: string,
     @Ctx() ctx: Context
   ): Promise<boolean> {
-    new ImageRepo(ctx, imageId).build();
+    await new ImageRepo(ctx, imageId).build();
     return true;
   }
 
