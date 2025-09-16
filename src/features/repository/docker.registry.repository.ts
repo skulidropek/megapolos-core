d02cf9b08a584fcccbbdf4502a27b9c959b43974
dilesoft <dilesoft@gmail.com>
2025-09-16 15:46:18 +0300

    makeem repository

--- src/features/repository/docker.registry.repository.ts
+++ src/features/repository/docker.registry.repository.ts
@@ -11,14 +11,8 @@ export default class DockerRegistryRepo extends BaseRepo<DockerRegistry> {
   async setAsDefault(): Promise<boolean> {
     const thisDocReg = await this.getEntity();
 
-    await makeEm().transactional(async (tx) => {
-      await tx.nativeUpdate(DockerRegistry, {}, { isDefault: false });
-      await tx.nativeUpdate(
-        DockerRegistry,
-        { id: thisDocReg.id },
-        { isDefault: true }
-      );
-    });
+    await makeEm().nativeUpdate(DockerRegistry, {}, { isDefault: false });
+    await this.update({ isDefault: true });
 
     return true;
   }
