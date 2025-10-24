175f2105348733498f1bb6269eefd9f6d16b4a73
dilesoft <dilesoft@gmail.com>
2025-10-24 14:14:54 +0300

    docker file

--- src/domain/config/config.docker.ts
+++ src/domain/config/config.docker.ts
@@ -1,9 +1,12 @@
 export default {
   secret: process.env.MEGAPOLOS_SECRET,
   connectionString: process.env.MEGAPOLOS_CONNECTION_STRING,
-  registryHost: process.env.MEGAPOLOS_REGISTRY_HOST,
-  registryUser: process.env.MEGAPOLOS_REGISTRY_USER,
-  registryPassword: process.env.MEGAPOLOS_REGISTRY_PASSWORD,
-  debug: process.env.MEGAPOLOS_DEBUG,
-  devMode: process.env.MEGAPOLOS_DEV_MODE,
+  registryHost: process.env.MEGAPOLOS_REGISTRY_HOST || '',
+  registryUser: process.env.MEGAPOLOS_REGISTRY_USER || '',
+  registryPassword: process.env.MEGAPOLOS_REGISTRY_PASSWORD || '',
+  debug: process.env.MEGAPOLOS_DEBUG || false,
+  devMode: process.env.MEGAPOLOS_DEV_MODE || false,
+  publicSchema: process.env.MEGAPOLOS_PUBLIC_SCHEMA || false,
+  allowUnauthorized: process.env.MEGAPOLOS_ALLOW_UNAUTHORIZED || false,
+  noRoot: process.env.MEGAPOLOS_NO_ROOT || false,
 };
